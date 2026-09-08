678bc868bbcb711ac3f8cac60c0b47b9c7b07ddd
JustinMelore <melorejustin@gmail.com>
2026-09-08 16:15:49 -0600

    Reconfigured project to connect development frontend to backend with some additional environment variables

--- backend/index.js
+++ backend/index.js
@@ -8,6 +8,16 @@ app.use(drinkRoutes);
 app.use(express.static("../frontend/dist"));
 app.use("/drinks", express.static("../frontend/dist"));
 
+function corsConfig(req, res, next) {
+    if(process.env.DEV_CORS_ORIGIN) {
+        res.set("Access-Control-Allow-Origin", process.env.DEV_CORS_ORIGIN);
+        console.log(`Allow origin: ${res.get("Access-Control-Allow-Origin")}`);
+    }
+    next();
+}
+
+app.use(corsConfig);
+
 app.listen(port, () => {
     data.connectToDatabase();
     console.log(`Server is now listening on port ${port}`);
