e2d2c854fd28496aaa7ae2243a464af05f3af82a
JustinMelore <melorejustin@gmail.com>
2026-09-09 20:51:08 -0600

    Added the ability to search for drinks using a query string in the backend API

--- backend/drinkRoutes.js
+++ backend/drinkRoutes.js
@@ -7,9 +7,22 @@ const data = require("./data");
 const drinkRoutes = express.Router();
 
 drinkRoutes.route("/api/drinks").get(async (req, res) => {
-    //TODO Add query string logic for filtering
-    console.log(`Allow origin in route: ${res.get("Access-Control-Allow-Origin")}`);
-    allDrinks = await data.getDatabase().collection("drinks").find().sort({name: 1}).toArray();
+    //TODO Allow search to work over ingredients and garnish as well
+    allDrinks = [];
+    if(req.query.search) {
+        let searchQuery = {
+            "$search": {
+                "index": "nameTextSearch",
+                "wildcard": {
+                    "query": `*${req.query.search}*`,
+                    "path": "name",
+                    "allowAnalyzedField": true
+                }
+            }
+        }
+        allDrinks = await data.getDatabase().collection("drinks").aggregate([searchQuery]).sort({name: 1}).toArray();
+    } else
+        allDrinks = await data.getDatabase().collection("drinks").find().sort({name: 1}).toArray();
     res.json(allDrinks);
 });
 
