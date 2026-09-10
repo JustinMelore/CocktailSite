8f65efc4517aad6874e7ad1df1a4a3224fdd3c60
JustinMelore <melorejustin@gmail.com>
2026-09-10 17:25:44 -0600

    Added the ability to search over the name, garnish, and ingredeints of a drink in the backend API

--- backend/drinkRoutes.js
+++ backend/drinkRoutes.js
@@ -7,25 +7,47 @@ const data = require("./data");
 const drinkRoutes = express.Router();
 
 drinkRoutes.route("/api/drinks").get(async (req, res) => {
-    //TODO Allow search to work over ingredients and garnish as well
     allDrinks = [];
     if(req.query.search) {
-        let searchQuery = {
-            "$search": {
-                "index": "nameTextSearch",
-                "wildcard": {
-                    "query": `*${req.query.search}*`,
-                    "path": "name",
-                    "allowAnalyzedField": true
-                }
-            }
-        }
-        allDrinks = await data.getDatabase().collection("drinks").aggregate([searchQuery]).sort({name: 1}).toArray();
+        const searchQuery = getSearchQuery(req.query.search);
+        allDrinks = await data.getDatabase().collection("drinks").aggregate([searchQuery]).toArray();
     } else
         allDrinks = await data.getDatabase().collection("drinks").find().sort({name: 1}).toArray();
     res.json(allDrinks);
 });
 
-
+/**
+ * Returns the completed search query over the drinks database using a provided search term
+ * @param {string} search - The search term to use with the query 
+ * @returns An object representing the search query. Should be used with the aggregate() function
+ */
+function getSearchQuery(search) {
+    const searchPaths = ["name", "garnish", "ingredients.ingredient"];
+    let singleQueries = searchPaths.map((elem) => {
+        return {
+            wildcard: {
+                query: `*${search}*`,
+                path: elem,
+                allowAnalyzedField: true
+            }
+        }
+    });
+    singleQueries[0].wildcard.score = {boost: { value: 2}};
+    singleQueries[2] = {
+        embeddedDocument: {
+            path: "ingredients",
+            operator: singleQueries[2]
+        }
+    }
+    return {
+        $search: {
+            index: "nameTextSearch",
+            compound: {
+                should: singleQueries,
+                minimumShouldMatch: 1
+            }
+        }
+    };
+}
 
 module.exports = drinkRoutes;
