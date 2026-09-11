b4115307f9fe7e8323d261b3cdef162e14144574
JustinMelore <melorejustin@gmail.com>
2026-09-10 19:37:33 -0600

    Updated style and layout of homepage

--- frontend/src/pages/home/Home.jsx
+++ frontend/src/pages/home/Home.jsx
@@ -1,5 +1,17 @@
+import Searchbar from "../../components/searchbar/Searchbar";
+import style from "./Home.module.css";
+import logo from "../../assets/logo.svg";
 export default function Home() {
     return (
-        <h1>HOME</h1>
+        <main className={style.home}>
+            <div className={style.dummy}>
+                <img src={logo}/>
+            </div>
+
+            <h1>Site Name Here</h1>
+            <div className={style.searchbarContainer}>
+                <Searchbar/>
+            </div>
+        </main>
     )
 }
