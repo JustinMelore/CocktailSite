c26919bc8f3175c031d7192259cf380903d316cc
JustinMelore <melorejustin@gmail.com>
2026-09-14 20:10:27 -0600

    Made it so cocktail cards use the image associated with the drink

--- frontend/src/components/drinkcard/DrinkCard.jsx
+++ frontend/src/components/drinkcard/DrinkCard.jsx
@@ -1,10 +1,34 @@
 import { Link } from "react-router";
 import style from "./DrinkCard.module.css";
+import { useState } from "react";
+import logo from "../../assets/logo.svg";
+import { useEffect } from "react";
 export default function DrinkCard({drinkObject}) {
+    
+    const [imgSrc, setImgSrc] = useState(logo);
+    
+    useEffect(() => {
+      async function getImage() {
+        try {
+          const url = import.meta.env.VITE_DEV_URL || "/";
+          const response = await fetch(url + "api/images/" + drinkObject.image);
+          if(!response.ok) {
+            return setImgSrc(logo);
+          }
+          const imgBlob = await response.blob();
+          setImgSrc(URL.createObjectURL(imgBlob));
+        } catch {
+          setImgSrc(logo);
+        }
+      }
+      if(drinkObject && drinkObject.image)
+        getImage();
+    }, [imgSrc, drinkObject]);
+
     return (
       <Link className={style.drinkCardLink} to={`/drinks/${drinkObject.name}`}>
         <article className={style.drinkCard}>
-          <img alt={`${drinkObject.name ? drinkObject.name : "Drink"} Image`}/>
+          <img alt={`${drinkObject.name ? drinkObject.name : "Drink"} Image`} src={imgSrc}/>
           <h2>{drinkObject && drinkObject.name ? drinkObject.name : "Drink Name"}</h2>
           <ul>
               {
